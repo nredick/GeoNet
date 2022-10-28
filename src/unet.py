7d3d9d23a6a5710d77dac40630a738ed02b1b092
Nathalie Redick <>
2022-10-28 17:33:44 -0400

    added structure through data aug

--- src/unet.py
+++ src/unet.py
@@ -20,23 +20,25 @@
 # # Imports & Environment Settings
 #
 # ## Imports
+#
 
 # %%
 # imports and setup
 import numpy as np
 import pandas as pd
-import geopandas as gpd 
+import geopandas as gpd
 import os
 import random
-import tensorflow as tf 
+import tensorflow as tf
 from keras import backend as K
 
 import ipywidgets as widgets
-
+from keras import backend as K
+import tensorflow as tf
 
 
 # %% [markdown]
-# ## Setting a random seed 
+# ## Setting a random seed
 #
 # ### What is a random seed?
 #
@@ -44,36 +46,118 @@ import ipywidgets as widgets
 #
 # In short, this allows the results of this workflow to be reproducible.
 #
-# Set the random seed to any integer value you please. 
+# Set the random seed to any integer value you please.
+#
 
 # %%
 # define a random seed for reproducibility
 seed_picker = widgets.IntText(
-    value=7,
+    value=42,
     description='Seed:',
     disabled=False
 )
 seed_picker
 
 
+
 # %%
-seed = seed_picker.value
+SEED = seed_picker.value
 
 # set `PYTHONHASHSEED` environment variable at a fixed value
-os.environ['PYTHONHASHSEED']=str(seed)
+os.environ['PYTHONHASHSEED'] = str(SEED)
 
 # set `python` built-in pseudo-random generator at a fixed value
-random.seed(seed)
+random.seed(SEED)
 
 # set `numpy` pseudo-random generator at a fixed value
-np.random.seed(seed)
+np.random.seed(SEED)
 
 # set `tensorflow` pseudo-random generator at a fixed value
-import tensorflow as tf
-tf.random.set_seed(seed)
+tf.random.set_seed(SEED)
 
 # configure a new global `tensorflow` session
-from keras import backend as K
-session_conf = tf.ConfigProto(intra_op_parallelism_threads=1, inter_op_parallelism_threads=1)
-sess = tf.Session(graph=tf.get_default_graph(), config=session_conf)
+session_conf = tf.compat.v1.ConfigProto(
+    intra_op_parallelism_threads=1, inter_op_parallelism_threads=1)
+sess = tf.compat.v1.Session(graph=tf.compat.v1.get_default_graph(), config=session_conf)
 K.set_session(sess)
+
+
+# %% [markdown]
+# # Add Data
+#
+# ## Upload your data
+
+# %%
+add_data = widgets.FileUpload(
+    accept='',  # todo: add file types 
+    multiple=True  
+)
+
+add_data
+
+# %% [markdown]
+# ## Optional: Automatically add DEM data
+#
+# Automatically download and add DEM data to your project. This will add a new layer to your project called `dem`.
+#
+# This data is sourced from todo
+#
+# > This only works if your area of interest is in the USA
+
+# %% [markdown]
+# # Preliminary Data Analysis
+
+# %%
+# todo add pandas profiling with options for in depth not in depth, to save or not save to file
+
+# %% [markdown]
+# # Data Preparation
+#
+# Create your dataset!
+#
+# ## Select your tile size 
+#
+# ### What is a tile size?
+#
+# ### Why are tile sizes powers of 2?
+#
+# ### How to choose a tile size
+
+# %%
+select_tile_size = widgets.Dropdown(
+    options=['16', '32', '64', '128', '256', '512'],
+    value='128',
+    description='Number:',
+    disabled=False,
+)
+select_tile_size
+
+# %%
+TILE_SIZE = int(select_tile_size.value)
+
+# %% [markdown]
+# ## Rasterize data 
+
+# %% [markdown]
+# ## Build a composite raster
+
+# %% [markdown]
+# ## Crop to tiles 
+
+# %% [markdown]
+# ## Optional: Data Augmentation
+#
+# > Defaults to `True`
+#
+# ### What is data augmentation?
+#
+# ### What kind of data augmentation should I use?
+
+# %%
+chkboxes = widgets.Checkbox(
+    value=False,
+    description='Check me',
+    disabled=False,
+    indent=False
+)
+chkboxes
